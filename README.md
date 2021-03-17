c0713480e1f2a4329954bc85a168a460e6ee56e6
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-03-16 21:56:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# API Personal Budget - Frontend 2021
+
+Esta aplicación construida con ReactJS 17 y corresponde al front-end de la API REST budget-backend.
+
+
---
+++ src/hooks/useGetOperations.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+
+export function useGetOperations() {
+    const [operations, setOpertions] = useState([]);
+
+    useEffect(() => {
+        
+    });
+}
